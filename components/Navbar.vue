9ef7d79a255783e77609393725316c699a3139e3
Ameer Izwan <mrblock7777@gmail.com>
2025-02-09 19:19:40 +0800

    Condensed menu to variables

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,14 +6,28 @@
                     Ameer Izwan
                 </div>
                 <div class="flex gap-x-10 text-xl font-[Montserrat]">
-                    <span>About</span>
-                    <span>Experience</span>
-                    <span>Education</span>
+                    <a :href="'/#'+section.id" v-for="section in sections">{{ section.name }}</a>
                 </div>
                 <div>
-                    <button class="border border-blue-500 rounded-sm px-4 py-2 bg-blue-500 text-white">Contact Me</button>
+                    <a href="/#contact" class="border border-blue-500 rounded-sm px-4 py-2 bg-blue-500 text-white">Contact Me</a>
                 </div>
             </nav>
         </div>
     </div>
 </template>
+<script setup lang="ts">
+const sections = ref([
+    {
+        name: 'About',
+        id: 'about'
+    },
+    {
+        name: 'Experience',
+        id: 'experience'
+    },
+    {
+        name: 'Education',
+        id: 'education'
+    }
+])
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  modules: ['@nuxtjs/google-fonts'],
+  modules: [
+    '@nuxtjs/google-fonts',
+    'nuxt-mdi'
+  ],
   googleFonts:{
     families:{
       Montserrat: [400, 900]
