0f513403411307191fee7680f76c42b8a8eb8b81
Ameer Izwan <mrblock7777@gmail.com>
2025-02-09 19:41:43 +0800

    Responsive design for navbar

---
+++ components/Dropdown.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="relative inline-block">
+        <button class="border border-gray-300 rounded-lg px-4 py-2 text-2xl" @click="show = !show">
+            <MdiIcon icon="mdiMenu" />
+        </button>
+        <div v-show="show" class="absolute">
+            <div class="flex flex-col border border-gray-300 rounded-lg bg-white text-xl font-[Montserrat]">
+                <slot></slot>  
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+const show = ref(false)
+</script>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="flex w-screen py-4">
         <div class="w-full px-20">
-            <nav class="border border-gray-200 rounded-lg px-4 py-6 w-full flex items-center justify-between">
-                <div class="flex-none text-4xl">
+            <nav class="border border-gray-200 rounded-lg px-4 py-6 w-full flex items-center justify-center md:justify-between">
+                <div class="flex-none text-xl md:text-4xl">
                     Ameer Izwan
                 </div>
-                <div class="flex gap-x-10 text-xl font-[Montserrat]">
-                    <a :href="'/#'+section.id" v-for="section in sections">{{ section.name }}</a>
+                <div class="mx-2 md:hidden">
+                    <Dropdown>
+                        <a class="hover:bg-gray-200 text-lg px-4 py-2" :href="'/#' + section.id" v-for="section in sections">{{ section.name }}</a>
+                    </Dropdown>
                 </div>
-                <div>
-                    <a href="/#contact" class="border border-blue-500 rounded-sm px-4 py-2 bg-blue-500 text-white">Contact Me</a>
+                <div class="hidden md:flex gap-x-10 text-xl font-[Montserrat]">
+                    <a :href="'/#'+section.id" v-for="section in sections">{{ section.name }}</a>
                 </div>
             </nav>
         </div>
@@ -28,6 +30,10 @@ const sections = ref([
     {
         name: 'Education',
         id: 'education'
+    },
+    {
+        name: 'Contact',
+        id: 'contact'
     }
 ])
 </script>
