b202720d8dc73df2f157f6b9e7e9e52ca6af2557
Ameer Izwan <mrblock7777@gmail.com>
2025-02-20 00:16:54 +0800

    Added dark mode support for dropdown

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="relative inline-block">
+    <div class="relative inline-block ">
         <button class="border border-gray-300 rounded-lg cursor-pointer px-4 py-2 text-2xl" @click="show = !show">
             <MdiIcon icon="mdiMenu" />
         </button>
         <div v-show="show" class="absolute">
-            <div class="flex flex-col border border-gray-300 rounded-lg bg-white text-xl font-[Montserrat]">
+            <div class="flex flex-col border border-gray-300 rounded-lg bg-white dark:bg-neutral-900 text-xl font-[Montserrat]">
                 <slot></slot>  
             </div>
         </div>
