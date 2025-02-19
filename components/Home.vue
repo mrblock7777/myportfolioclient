8f6f3b7b7e3f1b8cf53cb15d0367a8c308b2a68c
Ameer Izwan <mrblock7777@gmail.com>
2025-02-20 00:49:54 +0800

    Fixed layout on homepage

--- components/Home.vue
+++ components/Home.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="flex flex-col lg:flex-row lg:justify-between min-h-full h-screen lg:h-auto min-w-full justify-center items-center bg-[url('/_nuxt/assets/profile.jpg')] bg-cover bg-center bg-neutral-500 bg-blend-multiply lg:bg-transparent lg:bg-none  py-4" id="#">
-        <div>
-            <span class="text-white lg:dark:text-white lg:text-black font-[Montserrat] lg:text-9xl text-8xl">
+        <div class="flex flex-col items-center lg:flex lg:items-start">
+            <span class="text-center lg:text-left text-white lg:dark:text-white lg:text-black font-[Montserrat] lg:text-9xl text-8xl">
                 Ameer <br/>
                 Izwan
             </span>
+            <span class="text-white lg:text-black text-center lg:text-left text-2xl lg:dark:text-white w-3/5 block font-[Montserrat]">
+                Began my journey in Software Development in 2020, I am passionate creating and working things with technology. Experienced in Web Technologies and Cloud.
+            </span>
         </div>
-        <div class="dark:brightness-50 hidden lg:block w-1/2 h-auto">
+        <div class="dark:brightness-50 hidden lg:block w-3/4 h-auto">
             <img src="@/assets/profile.jpg" alt="Profile">
         </div>
     </div>
